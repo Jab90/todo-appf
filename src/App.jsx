8e938e68d8f3441fdf872821f5cbc49c79229e94
Jabbeer Jeerooburkhan <jabbeerjeerooburkhan@Jabbeers-iMac.local>
2023-06-07 22:24:39 +0100

    Added browser router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.scss'
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     );
   
 }
