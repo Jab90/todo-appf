b0de5a909c98906f5f88f5be5596b8ea870e3a7f
Jabbeer Jeerooburkhan <jabbeerjeerooburkhan@Jabbeers-iMac.local>
2023-06-08 10:48:29 +0100

    addtodo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Container from '../components/container/Container'
 import Header from '../components/header/Header'
+import Addtodo from '../components/add_todo/addtodo'
 
 export default function Home() { 
   return (
     <Container>
       <Header/>
-
+      <Addtodo/>
     </Container>
 
   )
